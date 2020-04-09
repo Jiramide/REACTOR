e9d0478a018eb1ae52168cccaee7c125f554e259
Jade Piramide <jadepiramidepogi@gmail.com>
2020-04-09 01:22:27 -0700

    Pushable! and a bunch of reorganizing my project folder.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     public Vector3 horizontalUnit = new Vector3(0.0f, 0.0f, 1.0f);
     public Vector3 verticalUnit = new Vector3(1.0f, 0.0f, 0.0f);
 
     public float dasLength = 0.2f;
     private bool isDASActive = false;
     private float lastInputConsumed = 0.0f;
-    
-    private void Update() {
+
+    private Pushable playerPushable;
+
+    private void Start()
+    {
+        playerPushable = GetComponent<Pushable>();
+    }
+
+    private void Update()
+    {
 
         float horiAxis = Input.GetAxisRaw("Horizontal");
         float vertAxis = Input.GetAxisRaw("Vertical");
@@ -39,7 +46,9 @@ public class Player : MonoBehaviour
             unitToConsume = verticalUnit;
         }
 
-        transform.Translate(axisToConsume * unitToConsume);
+        Vector3 movementVector = axisToConsume * unitToConsume;
+        playerPushable.Push(movementVector);
+
     }
 
 }
---
+++ Assets/Scripts/Pushable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushable : MonoBehaviour
+{
+
+    public bool Push(Vector3 movementVector)
+    {
+
+        RaycastHit hitInfo;
+        bool allowedToMove = true;
+
+        if (Physics.Raycast(transform.position, movementVector, out hitInfo, 1.0f))
+        {
+            GameObject objectHit = hitInfo.transform.gameObject;
+            if (objectHit.CompareTag("Pushable"))
+            {
+                allowedToMove = objectHit.GetComponent<Pushable>().Push(movementVector);
+            }
+            else if (objectHit.CompareTag("Wall"))
+            {
+                allowedToMove = false;
+            }
+        }
+
+        if (allowedToMove)
+        {
+            transform.Translate(movementVector);
+        }
+
+        return allowedToMove;
+    }
+
+}
