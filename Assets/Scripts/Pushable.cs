4cff0d261657d0f3d48310323fc2e6e8a0c14876
Jade Piramide <jadepiramidepogi@gmail.com>
2020-04-12 18:55:09 -0700

    Added carrying! A Pushable object on top of another Pushable object will get pushed in the same direction as the object carrying it.

--- Assets/Scripts/Pushable.cs
+++ Assets/Scripts/Pushable.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class Pushable : MonoBehaviour
 {
 
+    private bool PromptPush(GameObject objectToPush, Vector3 pushDirection)
+    {
+        return objectToPush.GetComponent<Pushable>()
+            ?.Push(pushDirection)
+            ?? false;
+    }
+
     public bool Push(Vector3 movementVector)
     {
 
-        RaycastHit hitInfo;
+        RaycastHit movementHit;
         bool allowedToMove = true;
 
-        if (Physics.Raycast(transform.position, movementVector, out hitInfo, 1.0f))
+        if (Physics.Raycast(transform.position, movementVector, out movementHit, 1.0f))
         {
-            GameObject objectHit = hitInfo.transform.gameObject;
-            Pushable objectPushable = objectHit.GetComponent<Pushable>();
-
-            allowedToMove = objectPushable?.Push(movementVector) ?? false;
+            GameObject objectHit = movementHit.transform.gameObject;
+            allowedToMove = PromptPush(objectHit, movementVector);
         }
 
         if (allowedToMove)
         {
             transform.Translate(movementVector);
+
+            RaycastHit carryInfo;
+            if (Physics.Raycast(transform.position, Vector3.up, out carryInfo, 1.0f))
+            {
+                GameObject objectCarrying = carryInfo.transform.gameObject;
+                PromptPush(objectCarrying, movementVector);
+            }
         }
 
         return allowedToMove;
